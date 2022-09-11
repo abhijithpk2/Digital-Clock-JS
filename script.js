c0dd16845acd188a32665e85e664cc6223e3edbc
Abhijith PK <pkabhijith1@gmail.com>
2022-09-11 12:32:24 +0530

    Added date also few css changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function displayTime(){
     var dateTime = new Date();
 
+    var date = dateTime.getDate();
+    var months = dateTime.getMonth()+1;
+    var years = dateTime.getFullYear();
+
     var hours = dateTime.getHours();
     var minutes = dateTime.getMinutes();
     var seconds = dateTime.getSeconds();
     var session = document.getElementById('session');
 
-    if(hours>12){
+    if(hours>11){
         session.innerHTML = 'PM';
     }
     else{
@@ -15,6 +19,10 @@ function displayTime(){
     if(hours>12){
         hours = hours - 12;
     }
+
+    document.getElementById('date').innerHTML = date;
+    document.getElementById('month').innerHTML = months;
+    document.getElementById('year').innerHTML = years;
 
     document.getElementById('hour').innerHTML = hours;
     document.getElementById('minute').innerHTML = minutes;
